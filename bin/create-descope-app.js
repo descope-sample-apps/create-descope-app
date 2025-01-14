ec25b97d3c7687aaf9e74dcf808146c5b1c51cc3
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-01-14 14:54:15 -0800

    Update create-descope-app.js

--- bin/create-descope-app.js
+++ bin/create-descope-app.js
@@ -3,11 +3,16 @@
 import { createApp } from '../lib/create-app.js';
 import { Command } from 'commander';
 import { DEFAULT_PROJECT_NAME } from '../lib/create-app.js';
+import packageJson from './package.json'
 
-const program = new Command();
+const program = new Command(packageJson.name);
 
 program
-  .version('1.0.0')
+  .version(
+    packageJson.version,
+    '-v, --version',
+    'Output the current version of create-descope-app'
+  )
   .argument('[project-name]', 'Name of your new project', DEFAULT_PROJECT_NAME)
   .option('-t, --template [template]', 'Select a template (next-js-sample-app, etc)')
   .action(async (projectName, options) => {
